efadc2ccdf454bd80919dc6520a6b14a97b5b162
yyjazsf <admin@yingyj.com>
2019-01-14 14:03:26 +0800

    update ga code

--- src/index.js
+++ src/index.js
@@ -9,21 +9,18 @@ export default (api, opts) => {
   const gaTpl = function (code) {
     return `
     (function(){ 
-      if (!location.port) {
-        (function (i, s, o, g, r, a, m) {
-          i['GoogleAnalyticsObject'] = r;
-          i[r] = i[r] || function () {
-              (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-          a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-          a.async = 1;
-          a.src = g;
-          m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-        ga('create', '${code}', 'auto');
-        ga('send', 'pageview');
-      }
+      var script = document.createElement('script');
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=${code}';
+      script.async = true;
+      script.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = () => {
+          window.dataLayer.push(arguments);
+        };
+        window.gtag('js', new Date());
+        window.gtag('config', '${code}');
+      };
+      document.getElementsByTagName('head')[0].appendChild(script);
     })();
   `;
   };
